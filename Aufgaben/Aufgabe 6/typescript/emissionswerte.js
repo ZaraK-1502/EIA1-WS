4856bcdc9a28376029e0a90a48d548d1aae243b8
Zara Kocak <zaraizel@gmail.com>
2021-11-19 19:12:18 +0100

    Update emissionswerte.js

--- Aufgaben/Aufgabe 6/typescript/emissionswerte.js
+++ Aufgaben/Aufgabe 6/typescript/emissionswerte.js
@@ -36,29 +36,27 @@ var Au08Au18pr = (au18 / au08 - 1) * 100;
 var Au08Au18kg = au18 - au08;
 /* Gesamte Welt */
 var welt = af18 + sa18 + eu18 + na18 + as18 + au18;
-/* Console */
-console.log('Die Emission von Afrika ist:' + af18 + 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' + AfWelt + '%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' + Af08Af18pr + '% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das' + Af08Af18kg + 'kg CO2');
-console.log('Die Emission von Süd Amerika ist:' + sa18 + 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' + SaWelt + '%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' + Sa08Sa18pr + '% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das' + Sa08Sa18kg + 'kg CO2');
-console.log('Die Emission von Europa ist:' + eu18 + 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' + EuWelt + '%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' + Eu08Eu18pr + '% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das' + Eu08Eu18kg + 'kg CO2');
-console.log('Die Emission von Nord Amerika ist:' + na18 + 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' + NaWelt + '%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' + Na08Na18pr + '% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das' + Na08Na18kg + 'kg CO2');
-console.log('Die Emission von Asien ist:' + as18 + 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' + AsWelt + '%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' + As08As18pr + '% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das' + As08As18kg + 'kg CO2');
-console.log('Die Emission von Austarlien ist:' + au18 + 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' + AuWelt + '%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' + Au08Au18pr + '% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das' + Au08Au18kg + 'kg CO2');
+
+/*Berechnung der Emissionen*/
+function emission(countryName, emissionen2018, emission2008) {
+    document.querySelector(".countryName").innerHTML = countryName;
+    document.querySelector(".countryName1").innerHTML = countryName;
+    document.querySelector(".emissionen2018").innerHTML = emissionen2018.toString();
+    document.querySelector(".relativeToWorld").innerHTML = Math.round(emissionen2018 / all * 100 * 100) / 100 + "%";
+    document.querySelector(".growthRate").innerHTML = Math.round((emissionen2018 - emission2008) / emission2008 * 100 * 100 / 100) + "%";
+    document.querySelector(".growthRateAbsolute").innerHTML = `${Math.round((emissionen2018 - emission2008) * 100) / 100}`;
+    document.querySelector(".chart").style.height = `${Math.round((emissionen2018 / all * 100 * 100) / 100)}%`;
+}
+;
+/*Funktion*/
+window.addEventListener("load", function () {
+    document.querySelector(".europe").addEventListener("click", function () { emission(countryEurope, europe2018, europe2008); });
+    document.querySelector(".northamerica").addEventListener("click", function () { emission(countryNorthamerica, northAmerica2018, northAmerica2008); });
+    document.querySelector(".southamerica").addEventListener("click", function () { emission(countrySouthamerica, southAmerica2018, southAmerica2008); });
+    document.querySelector(".africa").addEventListener("click", function () { emission(countryAfrica, africa2018, africa2008); });
+    document.querySelector(".asia").addEventListener("click", function () { emission(countryAsia, asia2018, asia2008); });
+    document.querySelector(".australia").addEventListener("click", function () { emission(countryAustralia, africa2018, africa2008); });
+});
+
+
 //# sourceMappingURL=emissionswerte.js.map
